fd295b8227839b1e84c835ec72401d2fca8b16f7
hu1won <71807924+hu1won@users.noreply.github.com>
2020-10-31 12:52:10 +0900

    Initial commit

---
+++ app/src/main/java/com/hu1won/mysql_android_json_test/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hu1won.mysql_android_json_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
